d84c2f3ee2bf6e5904da3b8b4ff7751ba25ca7c5
Muhtasim Ferdous <mferdous98@aol.com>
2020-07-05 18:51:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
+  lowest_value = nil 
+  lowest_key = nil 
 name_hash.each do |key,value|
+  if lowest_value == nil || lowest_value < value 
+    
+    
+    
   binding.pry
 end
 end
