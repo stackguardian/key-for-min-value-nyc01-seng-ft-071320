ff673248c0bdb3366984da0205446389b956cc9c
Muhtasim Ferdous <mferdous98@aol.com>
2020-07-05 18:43:13 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-name_hash.collect do |value|
+name_hash.each do |key,value|
 end
 end
