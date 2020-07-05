3c492b6d25d640013d8c4745447ceaad44af9b98
Muhtasim Ferdous <mferdous98@aol.com>
2020-07-05 18:57:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
   lowest_key = nil 
 name_hash.each do |key,value|
   if lowest_value == nil || lowest_value < value 
-    
-    
+    lowest_value = value 
+    lowest_key = key 
     
   binding.pry
 end
