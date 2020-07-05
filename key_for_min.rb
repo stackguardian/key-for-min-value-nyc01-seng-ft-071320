bb0e1fccfd98c6f3529301e37dd9e5f260d7653b
Muhtasim Ferdous <mferdous98@aol.com>
2020-07-05 19:05:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil 
   lowest_key = nil 
 name_hash.each do |key,value|
-  if lowest_value == nil || lowest_value < value 
+  if lowest_value == nil || lowest_value > value 
     lowest_value = value 
     lowest_key = key 
   end    
